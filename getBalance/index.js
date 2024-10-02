60f29b0788dbd69a4d161951f92d99adf8c5dda0
santiago villarreal arley <115122095+Villarley@users.noreply.github.com>
2024-10-01 19:24:20 -0600

    updating getBalance

--- getBalance/index.js
+++ getBalance/index.js
@@ -1,18 +1,21 @@
 import { ethers } from "ethers"
+import dotenv from "dotenv"
+dotenv.config()
 
 // Reemplaza con tu URL de Alchemy
-const alchemyUrl = 'https://eth-mainnet.g.alchemy.com/v2/U7OdE42Acl8VTbyu8AoR3vij_IbR_fyn';
+const alchemyUrl = process.env.SEPOLIA_URL;
 
 // Crear un proveedor usando la URL de Alchemy
 const provider = new ethers.JsonRpcProvider(alchemyUrl);
 
 // Indicar la cuenta de la que se quiere conocer el saldo
-const address = "0xDedBf9813E55cad604a035fa99A7DBed29E70359";
+const address = process.env.WALLET_ACCOUNT;
 
 // Obtener el saldo de la cuenta
 async function getBalance() {
   try {
     const balance = await provider.getBalance(address);
+    console.log(address);
     console.log("Saldo de la cuenta:", ethers.formatEther(balance), "ETH");
   } catch (e) {
     console.error("Error al obtener el saldo de la cuenta", e);
